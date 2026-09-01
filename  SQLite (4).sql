4ab0b3bda92ae0990b94dc49d9fae1b3c2f0103d
faby <fabianyevellyn9@gmail.com>
2026-09-01 08:12:20 -0300

    Add files via upload

---
+++  SQLite (4).sql
@@ -0,0 +1,25 @@
+--- 01-09-2026 08:01:01 SQLite
+CREATE TABLE CATEGORIA (
+    id INTEGER PRIMARY KEY,
+    nome TEXT
+);
+
+CREATE TABLE PRODUTOS (
+    id INTEGER PRIMARY KEY,
+    nome TEXT,
+    preco REAL,
+    estoque INTEGER,
+    id_categoria INTEGER,
+    FOREIGN KEY (id_categoria) REFERENCES CATEGORIA(id)
+);
+
+INSERT INTO CATEGORIA (id, nome)
+VALUES (1, 'Ferramentas');
+
+INSERT INTO PRODUTOS (id, nome, preco, estoque, id_categoria)
+VALUES (1, 'Jogo de Chaves Soquete', 159.90, 20, 1);
+
+SELECT * FROM CATEGORIA;
+SELECT * FROM PRODUTOS;
+
+
---
+++ SQLite.sql
@@ -0,0 +1,73 @@
+--- 01-09-2026 07:52:50 SQLite
+CREATE TABLE PRODUTO (
+    id INTEGER PRIMARY KEY,
+    nome TEXT NOT NULL,
+    preco REAL
+);
+
+INSERT INTO PRODUTO VALUES (1, 'Furadeira de Impacto', 299.90);
+
+
+--- 01-09-2026 07:53:06 SQLite
+CREATE TABLE ALUNO (
+    id INTEGER PRIMARY KEY,
+    nome TEXT
+);
+
+ALTER TABLE ALUNO ADD COLUMN email TEXT;
+
+INSERT INTO ALUNO VALUES (10, 'Carlos Andrade', 'carlos@email.com');
+
+
+--- 01-09-2026 07:53:23 SQLite
+CREATE TABLE LIVRO (
+    id INTEGER PRIMARY KEY,
+    titulo TEXT NOT NULL,
+    autor TEXT
+);
+
+ALTER TABLE LIVRO ADD COLUMN ano_publicacao INTEGER;
+
+INSERT INTO LIVRO VALUES (1, 'O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 1943);
+
+
+--- 01-09-2026 07:59:57 SQLite
+CREATE TABLE ALUNOS (
+    id INTEGER PRIMARY KEY,
+    nome TEXT
+);
+
+ALTER TABLE ALUNOS
+ADD COLUMN email TEXT;
+
+INSERT INTO ALUNOS (id, nome, email)
+VALUES (10, 'Carlos Andrade', 'carlos@email.com');
+
+SELECT * FROM ALUNOS;
+
+
+--- 01-09-2026 08:01:01 SQLite
+CREATE TABLE CATEGORIA (
+    id INTEGER PRIMARY KEY,
+    nome TEXT
+);
+
+CREATE TABLE PRODUTOS (
+    id INTEGER PRIMARY KEY,
+    nome TEXT,
+    preco REAL,
+    estoque INTEGER,
+    id_categoria INTEGER,
+    FOREIGN KEY (id_categoria) REFERENCES CATEGORIA(id)
+);
+
+INSERT INTO CATEGORIA (id, nome)
+VALUES (1, 'Ferramentas');
+
+INSERT INTO PRODUTOS (id, nome, preco, estoque, id_categoria)
+VALUES (1, 'Jogo de Chaves Soquete', 159.90, 20, 1);
+
+SELECT * FROM CATEGORIA;
+SELECT * FROM PRODUTOS;
+
+
